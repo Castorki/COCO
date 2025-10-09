7535e2de6d1d223482ed7840c54e32b7aaed8c64
Darya Kauzova <amayasuzuki12@gmail.com>
2025-10-09 14:25:18 +0300

    created our specialisation component for about page

--- src/components/pages/aboutPage/AboutPage.js
+++ src/components/pages/aboutPage/AboutPage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './AboutPage.css';
 import { AudienceAttention } from './AudienceAttention';
 import { OurBeliefs } from './OurBeliefs';
+import { OurSpecialization } from './OurSpecialization';
 
 export const AboutPage = () => {
     return (
         <div id='about'>
             <AudienceAttention />
             <OurBeliefs />
+            <OurSpecialization />
         </div>
     )
 }
---
+++ src/components/pages/aboutPage/OurSpecialization.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const OurSpecialization = () => {
+    return (
+        <div className='ourSpecialization center'>
+            <h2 className='ourSpecialization__title'>Powerful ideas & sales acceleration</h2>
+            <p className='ourSpecialization__article'>
+                We are a software development firm specializing in digital products. We build beautiful, functional and cross-platform solutions that are accessible to all.
+            </p>
+        </div>
+    )
+}
