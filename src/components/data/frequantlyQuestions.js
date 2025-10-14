71b11a522724f514d2d171a050979a9dba2596e0
Darya Kauzova <amayasuzuki12@gmail.com>
2025-10-14 21:20:22 +0300

    completed pricing page

--- src/components/data/frequantlyQuestions.js
+++ src/components/data/frequantlyQuestions.js
@@ -3,5 +3,25 @@ export const questions = [
         id: 1,
         title: "Is there a free trial available?",
         article: "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
-    }
+    },
+    {
+        id: 2,
+        title: "How Can I Add More Emails To My Account?",
+        article: "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
+    },
+    {
+        id: 3,
+        title: "Can I Change Plans Or Cancel My Subscription At Any Time?",
+        article: "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
+    },
+    {
+        id: 4,
+        title: "How Secure Is My Date With Sans CRM?",
+        article: "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
+    },
+    {
+        id: 5,
+        title: "What Is The Uptime Guarantee?",
+        article: "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
+    },
 ]
--- src/components/pages/pricingPage/FrequantlyQuestions.js
+++ src/components/pages/pricingPage/FrequantlyQuestions.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+
 
 
 export const FrequantlyQuestions = () => {
 
-    const useAccordion = () => {
 
-        const [openIndex, setOpenIndex] = useState(null);
+    const questions = useSelector(state => state.questions);
+    const [openIndex, setOpenIndex] = useState(null);
 
-        const toggleItem = (index) => {
-            setOpenIndex(openIndex === index ? null : index)
-        }
-
-        return (openIndex, toggleItem);
-    }
+    const toggleItem = (index) => {
+        console.log(index);
+        setOpenIndex(openIndex === index ? null : index);
+    };
 
 
     return (
@@ -21,15 +21,27 @@ export const FrequantlyQuestions = () => {
                 <h2 className='frequantlyQuestions__header_title'>Frequently asked questions</h2>
                 <p className='frequantlyQuestions__header_article'>Everything you need to know about the product and billing.</p>
             </div>
+            {questions.map((question, index) => (
+                <div key={question.id} className='frequantlyQuestions__question'>
+                    <h3 className='frequantlyQuestions__question_title'>{question.title}</h3>
+                    <p className={`frequantlyQuestions__question_article ${openIndex === index ? `open` : ``}`}>
+                        {question.article}
+                    </p>
+                    <button className='frequantlyQuestions__question_button'
+                        onClick={() => toggleItem(index)}>
+                        <img src={openIndex === index ? `frequantlyQuestionButtonOpen.svg` : `frequantlyQuestionButtonClose.svg`}></img>
+                    </button>
+                </div>
 
-           
-            <div className='frequantlyQuestions__question'>
+            ))
+            }
+            {/* <div className='frequantlyQuestions__question'>
                 <h3 className='frequantlyQuestions__question_title'>Is there a free trial available?</h3>
-                {/* <p className='frequantlyQuestions__question_article'>
+                <p className='frequantlyQuestions__question_article'>
                     Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.
-                </p> */}
+                </p>
                 <button className='frequantlyQuestions__question_button'><img src='frequantlyQuestionButtonClose.svg'></img></button>
-            </div>
-        </div>
+            </div> */}
+        </div >
     )
 }
