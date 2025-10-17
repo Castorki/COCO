c40c92019da35d5cfb007738d0d2d9fb55e2f095
Darya Kauzova <amayasuzuki12@gmail.com>
2025-10-17 14:10:16 +0300

    added contact form for Contacts page

--- src/components/pages/contactsPage/ContactForm.js
+++ src/components/pages/contactsPage/ContactForm.js
@@ -12,31 +12,33 @@ export const ContactForm = () => {
                 </div>
                 <div className='form__userData'>
                     <div className='form__userData_firstName'>
-                        <label className='form__userData_firstName label' for='firstName'>First name</label>
-                        <input className='form__userData_firstName input' type='text' id='name' required></input>
+                        <label className='form__userData_firstName label' htmlFor='firstName'>First name</label>
+                        <input className='form__userData_firstName input' type='text' id='firstName' placeholder='First name' required></input>
                     </div>
                     <div className='form__userData_lastName'>
-                        <label className='form__userData_lastName label' for='lastName'>Last name</label>
-                        <input className='form__userData_lastName input' type='text' id='name' required></input>
+                        <label className='form__userData_lastName label' htmlFor='lastName'>Last name</label>
+                        <input className='form__userData_lastName input' type='text' id='lastName' placeholder='Last name' required></input>
                     </div>
                 </div>
                 <div className='form__userEmail'>
-                    <label className='form__userEmail_label' for='email'>Email</label>
-                    <input className='form__userEmail_input' type='email' id='email' required></input>
+                    <label className='form__userEmail_label' htmlFor='email'>Email</label>
+                    <input className='form__userEmail_input' type='email' id='email' placeholder='you@company.com' required></input>
                 </div>
                 <div className='form__userPhoneNumber'>
-                    <label className='form__userPhoneNumber_label' for='phoneNumber'>Phone number</label>
-                    <input className='form__userPhoneNumber_input' type='number' id='phoneNumber' required></input>
+                    <label className='form__userPhoneNumber_label' htmlFor='phoneNumber'>Phone number</label>
+                    <input className='form__userPhoneNumber_input' type='number' id='phoneNumber' placeholder='+1 (555) 000-0000' required></input>
                 </div>
                 <div className='form__userMessage'>
-                    <label className='form__userMessage_label' for='message'>Message</label>
-                    <input className='form__userMessage_input' type='text' id='message'></input>
+                    <label className='form__userMessage_label' htmlFor='message'>Message</label>
+                    <textarea className='form__userMessage_textArea' type='text' id='message' placeholder='Leave us a message...'></textarea>
                 </div>
-                <div className='form__submitButton'>
-                    <input className='form__submitButton_input' type="submit" value="Get Started" />
-                </div>
+                <button className='form__submitButton' type='submit'>
+                    Get Started
+                </button>
             </form>
-            <img className='contactsForm__photo' src='contactsFormPicture.png'></img>
+            <div className='contactsForm__wrapper'>
+            <img className='contactsForm__wrapper_photo' src='contactsFormPicture.png'></img>
+            </div>
         </div>
     )
 }
